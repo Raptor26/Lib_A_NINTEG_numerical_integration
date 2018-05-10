7b3045a89997966f20691e705e7bbb41fffa7297
Isaev Mickle <mrraptor26@gmail.com>
2018-05-10 17:44:04 +0300

    chore (Lib_A_NINTEG_numerical_integration)

--- Lib_A_NINTEG_numerical_integration.c
+++ Lib_A_NINTEG_numerical_integration.c
@@ -81,44 +81,47 @@
 
 /*============================================================================*/
 void NINTEG_IntegrateAngleVelocityTrapeziumAllAxis(
-                                                   float *pPreviousAngleVelocityArr,
-                                                   float *pCurrentAngleVelocityArr,
-                                                   float deltaTimeInSec,
-                                                   float *pDeltaAnglesArr)
+    float *pPreviousAngleVelocityArr,
+    float *pCurrentAngleVelocityArr,
+    float deltaTimeInSec,
+    float *pDeltaAnglesArr)
 {
-    //  Нахождение дельты угла по оси X;
-    *pDeltaAnglesArr++ = NINTEG_IntegrateAnglVelocityTrapezium \
-                         (pPreviousAngleVelocityArr++, \
-                          pCurrentAngleVelocityArr++, \
-                          deltaTimeInSec);
+	//  Нахождение дельты угла по оси X;
+	*pDeltaAnglesArr++ = NINTEG_IntegrateAnglVelocityTrapezium
+	                     (
+	                     pPreviousAngleVelocityArr++,
+	                     pCurrentAngleVelocityArr++,
+	                     deltaTimeInSec);
 
-    //  Нахождение дельты угла по оси Y;
-    *pDeltaAnglesArr++ = NINTEG_IntegrateAnglVelocityTrapezium \
-                         (pPreviousAngleVelocityArr++, \
-                          pCurrentAngleVelocityArr++, \
-                          deltaTimeInSec);
+	//  Нахождение дельты угла по оси Y;
+	*pDeltaAnglesArr++ = NINTEG_IntegrateAnglVelocityTrapezium
+	                     (
+	                     pPreviousAngleVelocityArr++,
+	                     pCurrentAngleVelocityArr++,
+	                     deltaTimeInSec);
 
-    //  Нахождение дельты угла по оси Z;
-    *pDeltaAnglesArr = NINTEG_IntegrateAnglVelocityTrapezium \
-                         (pPreviousAngleVelocityArr, \
-                          pCurrentAngleVelocityArr, \
-                          deltaTimeInSec);
+	//  Нахождение дельты угла по оси Z;
+	*pDeltaAnglesArr = NINTEG_IntegrateAnglVelocityTrapezium
+	                   (
+	                   pPreviousAngleVelocityArr,
+	                   pCurrentAngleVelocityArr,
+	                   deltaTimeInSec);
 }
 
 float NINTEG_IntegrateAnglVelocityTrapezium(
-                                            float *pPreviousAngleVelocity,
-                                            float *pCurrentAngleVelocity,
-                                            float deltaTimeInSec)
+    float *pPreviousAngleVelocity,
+    float *pCurrentAngleVelocity,
+    float deltaTimeInSec)
 {
-    //  Нахождение угла на основе текущей угловой скорости и угловой скорости в
-    //  предыдущий момент времени;
-    float deltaAngel = (*pPreviousAngleVelocity + *pCurrentAngleVelocity) * deltaTimeInSec / 2.0f;
+	//  Нахождение угла на основе текущей угловой скорости и угловой скорости в
+	//  предыдущий момент времени;
+	float deltaAngel = (*pPreviousAngleVelocity + *pCurrentAngleVelocity) * deltaTimeInSec / 2.0f;
 
-    //  Копирование текущей угловой скорости в переменную угловой скорости за
-    //  предыдущий момент времени;
-    *pPreviousAngleVelocity = *pCurrentAngleVelocity;
+	//  Копирование текущей угловой скорости в переменную угловой скорости за
+	//  предыдущий момент времени;
+	*pPreviousAngleVelocity = *pCurrentAngleVelocity;
 
-    return deltaAngel;
+	return deltaAngel;
 }
 
 /**
@@ -132,18 +135,21 @@ float NINTEG_IntegrateAnglVelocityTrapezium(
  *      @note   (Приращение за промежуток времени между вызовами функии
  *              "NINTEG_FindDeltaTrapezium()");
  */
-float NINTEG_FindDeltaTrapezium(
-                                NINTEG_find_delta_trapezium_s *pStruct,
-                                float newData)
+float NINTEG_FindDeltaTrapezium
+(NINTEG_find_delta_trapezium_s *pStruct,
+    float newData)
 {
-    // Численное интегрирование методом трапеций;
-    pStruct->deltaData = (pStruct->previousData + newData) * pStruct->dT / 2.0f;
+	// Численное интегрирование методом трапеций;
+	pStruct->deltaData
+	= (pStruct
+	   ->previousData + newData) * pStruct->dT / 2.0f;
+	
+	// Копирование текущего значения переменной в переменную данных за предыдущий момент времени;
+	pStruct
+	->previousData = newData;
 
-    // Копирование текущего значения переменной в переменную данных за предыдущий момент времени;
-    pStruct->previousData = newData;
-
-    // Возврат интегрированного значения;
-    return pStruct->deltaData;
+	// Возврат интегрированного значения;
+	return pStruct->deltaData;
 }
 
 /**
